7d6d40a3d78b73c1d9eacb3abe756f62fc244f83
Przemyslaw Pajak <office@fearlessspider.com>
2021-04-23 10:56:43 +0200

    filter

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -18,3 +18,14 @@ squered = map(lambda num: num ** 2, numbers)
 
 print(squered)
 print(list(squered))
+
+import math
+
+def is_positive(num):
+    return num >= 0
+
+def sanitized_sqrt(numbers):
+    cleaned_iter = map(math.sqrt, filter(is_positive, numbers))
+    return list(cleaned_iter)
+
+print(sanitized_sqrt([25, 9, 81, -16, 0]))
