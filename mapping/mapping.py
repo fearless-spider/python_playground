038c491d032570c75a51f631d62cfed2cb0bbe49
Przemyslaw Pajak <office@fearlessspider.com>
2021-04-23 10:44:58 +0200

    map mapping

---
+++ mapping/mapping.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4,5]
+squered = []
+
+for number in numbers:
+    squered.append(number**2)
+
+print(squered)
+
+def square(number):
+    return number ** 2
+
+squered = map(square, numbers)
+
+print(squered)
+print(list(squered))
