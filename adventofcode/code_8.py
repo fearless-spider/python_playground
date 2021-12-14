71655c3cfb9e18b439316ffc90e18a2a37995ede
Przemysław Pająk <office@fearlessspider.com>
2021-12-14 02:03:17 +0100

    Star 8 - Day 4

--- adventofcode/code_8.py
+++ adventofcode/code_8.py
@@ -3,9 +3,9 @@ Lines = file1.readlines()
 
 bingo_numbers = Lines[0].split(",")
 bingo_tables = []
-bingo_win = 0
-bingo_row_number = 0
 bingo_tables_all = []
+found_bingo = []
+found_bingo_number = []
 for line in Lines[2:]:
     new_row = []
     if line == '\n':
@@ -19,3 +19,37 @@ for line in Lines[2:]:
         new_row.append(int(element.strip()))
     bingo_tables.append(new_row)
 
+for number in bingo_numbers:
+    for i in range(0, len(bingo_tables_all)):
+        if i not in found_bingo:
+            bingo = False
+            for j in range(0, 5):
+                total = 0
+                for k in range(0, 5):
+                    if int(number) == bingo_tables_all[i][j][k]:
+                        bingo_tables_all[i][j][k] = 0
+                    total += bingo_tables_all[i][j][k]
+                if total == 0:
+                    found_bingo.append(i)
+                    found_bingo_number.append(int(number))
+                    bingo = True
+                    break
+                if bingo:
+                    continue
+                total = 0
+                for k in range(0, 5):
+                    total += bingo_tables_all[i][k][j]
+                if total == 0:
+                    found_bingo.append(i)
+                    found_bingo_number.append(int(number))
+                    break
+
+print(found_bingo)
+print(bingo_tables_all[found_bingo[-1]])
+summary = 0
+for i in range(0,5):
+    for j in range(0,5):
+        summary += bingo_tables_all[found_bingo[-1]][i][j]
+print(summary)
+print(found_bingo_number[-1])
+print(summary*found_bingo_number[-1])
